a2198013010e29656381b5fddd750d89988cbbfc
dietotter <nikibar4@gmail.com>
2017-05-22 01:46:58 +0300

    HW1: Colliders, movement, camera follow

---
+++ Assets/src/HeroRabbit.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroRabbit : MonoBehaviour {
+
+    public int speed = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+           
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+            sr.flipX = true;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+            sr.flipX = false;
+        }
+    }
+}
